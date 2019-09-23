7cb4f9bc0808fdbef08175226bf073160493912d
Ranosys <soodan.madhu@ranosys.com>
2019-09-23 14:36:41 +0530

    => file name chenged !!

--- index.js
+++ index.js
@@ -17,24 +17,25 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
+// mongoose.connect(dbConfig.url, {
+//     useNewUrlParser: true
+// }).then(() => {
+//     console.log("Successfully connected to the database");
+// }).catch(err => {
+//     console.log('Could not connect to the database. Exiting now...', err);
+//     process.exit();
+// });
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
+    console.log({req, res})
+    res.json({name: "Madhu Sudan"});
 });
 
 // Require Notes routes
 require('./app/routes/note.routes.js')(app);
 
 // listen for requests
-app.listen(3001, () => {
-    console.log("Server is listening on port 3001");
+app.listen(3002, () => {
+    console.log("Server is listening on port 3002");
 });
